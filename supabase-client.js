d239b568af62e22ded8fc8f8155e2791e37b1478
samthierno19-blip <samthierno19@gmail.com>
2026-09-09 00:34:50 +0000

    Add Supabase client configuration

---
+++ supabase-client.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL='https://jqqfipqlvzvuuskbmmzn.supabase.co';
+const SUPABASE_KEY='sb_publishable_cMPXKmS0PzcbmHAN1Pex8g_CIz24ZdG';
+window.sameShopSupabase=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
