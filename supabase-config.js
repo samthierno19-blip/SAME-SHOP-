0d8270ce508daf9a7be577bb16d17176e1fce4e0
samthierno19-blip <samthierno19@gmail.com>
2026-09-10 01:54:25 +0000

    Add Supabase public configuration

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://jqqfipqlvzvuuskbmmzn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_cMPXKmS0PzcbmHAN1Pex8g_CIz24ZdG";
+window.sameShopSupabaseConfig = { SUPABASE_URL, SUPABASE_ANON_KEY };
